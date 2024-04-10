9fac356b5342635a1ac103192c40778314c38f40
Yepavlov <ff2-15@ukr.net>
2024-04-09 20:28:38 -0700

    HW_12 with corrected task with bug searching

--- HW_12/Corrected_code_from_the_bug.py
+++ HW_12/Corrected_code_from_the_bug.py
@@ -1,43 +1,23 @@
 """
-The bug was in 'else' case, the code didn't check the case when both parameters were entered!
-Below is the corrected code.
+There is a bug in def age, the function does not take into account the case when the student
+has not yet had a birthday this year, but the result of the function will be the same
+as if the student already had a birthday.
+The corrected code is given below.
 """
+from datetime import date
 
 
-class UserRegistrationForm(UserCreationForm):
-    class Meta:
-        model = get_user_model()
-        fields = [
-            "email",
-            "phone_number",
-            "password1",
-            "password2",
-            "first_name",
-            "last_name",
-        ]
+class Student(Person):
+    uuid = models.UUIDField(default=uuid4, primary_key=True, unique=True, editable=False, db_index=True)
+    group = models.ForeignKey("students.Group", on_delete=models.CASCADE)
 
-    def clean(self):
-        _clean_data = super().clean()
+    # def age(self):
+    #     return datetime.datetime.now().year - self.birth_date.year
 
-        if not bool(self.cleaned_data["email"]) and not bool(self.cleaned_data["phone_number"]):
-            raise ValidationError("Insert email or phone number. At least one of them should be set.")
-
-        elif not bool(self.cleaned_data["email"]):
-            if get_user_model().objects.filter(phone_number=_clean_data["phone_number"]).exists():
-                raise ValidationError("User with phone number already exists!!!")
-
-        elif not bool(self.cleaned_data["phone_number"]):
-            if get_user_model().objects.filter(email=_clean_data["email"]).exists():
-                raise ValidationError("User with email already exists!!!")
-
-        else:
-            email_exists = get_user_model().objects.filter(email=_clean_data["email"]).exists()
-            phone_number_exists = get_user_model().objects.filter(phone_number=_clean_data["phone_number"]).exists()
-            if email_exists and phone_number_exists:
-                raise ValidationError("User with these email and phone number already exists!!!")
-            elif email_exists:
-                raise ValidationError("User with this email already exists!!!")
-            elif phone_number_exists:
-                raise ValidationError("User with this phone number already exists!!!")
-
-        return _clean_data
+    def age(self):
+        today = date.today()
+        return (
+                today.year
+                - self.birth_date.year
+                - ((today.month, today.day) < (self.birth_date.month, self.birth_date.day))
+        )
